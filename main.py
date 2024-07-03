3a6b526b2201b1db52517308d20b93b6fab2f70c
yashushetty6110@gmail.com <yashushetty6110@gmail.com>
2024-07-03 09:15:19 +0530

    changes to main

--- main.py
+++ main.py
@@ -6,15 +6,9 @@ from division import divide
 def main():
     print("Simple Calculator Program")
     
-<<<<<<< HEAD
-    a = 5
-    b = 8
-    
-=======
     a = 15
     b = 17
 
->>>>>>> main
     print(f"Addition of {a} and {b}: {add(a, b)}")
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
     print(f"Multiplication of {a} and {b}: {multiply(a, b)}")
