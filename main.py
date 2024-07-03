9535bfa0a7f605b6874df4694e02b74a8903c5c4
yashushetty6110@gmail.com <yashushetty6110@gmail.com>
2024-07-03 08:52:52 +0530

    new message

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from division import divide
 def main():
     print("Simple Calculator Program")
     
-    a = 49
-    b = 8
+    a = 10
+    b = 18
 
     print(f"Addition of {a} and {b}: {add(a, b)}")
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
